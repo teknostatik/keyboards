cea9569d885e52530fa918313f012bf5989579ff
Andy Ferguson <andy@teknostatik.org>
2022-07-13 09:20:31 +0100

    New key for multi-line editing

--- corne/keymap.c
+++ corne/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL, KC_4, KC_5, KC_6, XXXXXXX, XXXXXXX,                      KC_F6, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, KC_7, KC_8, KC_9, KC_0, XXXXXXX,                      KC_F12, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TILD,
+      LCS(KC_DOWN), KC_7, KC_8, KC_9, KC_0, XXXXXXX,                      KC_F12, KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, KC_TILD,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT,   MO(3),  KC_SPC,     KC_ENT, _______, KC_LGUI
                                       //`--------------------------'  `--------------------------'
