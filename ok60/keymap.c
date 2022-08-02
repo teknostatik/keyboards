7a2b506ef93353d91503aed64ec76e8b448ceb43
Andy Ferguson <andy@teknostatik.org>
2022-08-02 20:36:05 +0100

    Adding some modtap

--- ok60/keymap.c
+++ ok60/keymap.c
@@ -5,9 +5,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_60_iso_split_rshift(
         KC_ESC,  KC_1,    KC_2,    KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
-        KC_CAPS, KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
+        MT(MOD_LCTL, KC_NUHS), KC_A,    KC_S,    KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,
         KC_LSFT, KC_GRAVE, KC_Z,    KC_X,    KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT,KC_ENT),MO(1),
-        KC_LCTL, MO(1), KC_LALT,               KC_SPC,                        MT(MOD_LALT,KC_LEFT), MT(MOD_LGUI,KC_DOWN), KC_UP, MT(MOD_RCTL,KC_RGHT)
+        MT(MOD_LCTL, KC_F5, MO(1), KC_LALT,               KC_SPC,                        MT(MOD_LALT,KC_LEFT), MT(MOD_LGUI,KC_DOWN), MT(MOD_RALT,KC_UP), MT(MOD_RCTL,KC_RGHT)
     ),
 
     [1] = LAYOUT_60_iso_split_rshift(
