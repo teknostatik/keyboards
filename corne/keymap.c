e60ff248816194dbea0bae3b2764c8b12fab1cbd
Andy Ferguson <andy@teknostatik.org>
2022-09-18 09:31:20 +0100

    Function keys

--- corne/keymap.c
+++ corne/keymap.c
@@ -35,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, LCA(KC_DEL),
+       KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, LCA(KC_DEL),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_PSCR, XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_PSCR,
+      KC_LCTL, KC_F5, KC_F6,  KC_F7,  KC_F8, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, KC_PSCR,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, KC_F5,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END, XXXXXXX, KC_F11,
+      KC_LSFT, KC_F9, KC_F10, KC_F11,  KC_F12, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP, KC_END, XXXXXXX, KC_F11,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT, _______,  KC_ENT,     KC_ENT,   MO(3), KC_LGUI
                                       //`--------------------------'  `--------------------------'
