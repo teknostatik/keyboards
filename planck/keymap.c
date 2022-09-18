9998572f4860210ea9a2a15de5b6e40989a10776
Andy Ferguson <andy@teknostatik.org>
2022-09-18 09:42:17 +0100

    KC_APP

--- planck/keymap.c
+++ planck/keymap.c
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,   KC_BSPC,
     MT(MOD_LCTL, KC_NUHS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     MT(MOD_LSFT, KC_NUBS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-    KC_LEFT, KC_RGHT, MT(MOD_LGUI, KC_PAST), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LCTL, KC_F5), KC_MENU, KC_DOWN, KC_UP
+    KC_LEFT, KC_RGHT, MT(MOD_LGUI, KC_PAST), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LCTL, KC_F5), KC_APP, KC_DOWN, KC_UP
 ),
 
 /* Colemak
