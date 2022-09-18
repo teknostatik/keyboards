7736f4a7d5f272e61cce508f77c3a2e347e95a1c
Andy Ferguson <andy@teknostatik.org>
2022-09-18 09:14:29 +0100

    Remap to match Corne exactly

--- planck/keymap.c
+++ planck/keymap.c
@@ -55,9 +55,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,   KC_BSPC,
-    KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    MT(MOD_LCTL, KC_NUHS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     MT(MOD_LSFT, KC_NUHS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-    MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT, LOWER, LT(LOWER, KC_SPC), KC_SPC, LT(RAISE, KC_SPC), MT(MOD_LALT, KC_LEFT), LAG_T(KC_DOWN), MT(MOD_LCTL, KC_UP), MT(MOD_LCTL, KC_RGHT)
+    KC_LEFT, KC_RGHT, MT(MOD_LGUI, KC_PAST), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LCTL, KC_F5), KC_MENU, KC_DOWN, KC_UP
 ),
 
 /* Colemak
@@ -108,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, LCA(KC_DEL),
+    KC_ESC, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, LCA(KC_DEL),
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F6,   KC_LEFT,   KC_DOWN,    KC_UP,    KC_RGHT, KC_RCBR, KC_PSCR,
     _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END, KC_END,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
