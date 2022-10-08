8fdb488a26edcd8ce0a1317b4ca33719573a6bea
Andy Ferguson <andy@teknostatik.org>
2022-10-08 19:10:05 +0100

    Copy bottom 4 rows from Planck

--- preonic/keymap.c
+++ preonic/keymap.c
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_preonic_grid(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
-  MT(MOD_LCTL, KC_NUHS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,   KC_BSPC,
+  MT(MOD_LCTL, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   MT(MOD_LSFT, KC_NUBS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LALT, KC_LEFT), LAG_T(KC_DOWN), MT(MOD_LCTL, KC_UP), MT(MOD_LCTL, KC_RGHT)
+   KC_LEFT, KC_RGHT, MT(MOD_LGUI, KC_PAST), KC_LALT, LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LCTL, KC_F5), KC_LALT, KC_DOWN, KC_UP
 ),
 
 /* Colemak
@@ -115,10 +115,10 @@ MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC
  */
 [_LOWER] = LAYOUT_preonic_grid(
   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, LCA(KC_DEL),
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PSCR,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),KC_HOME, KC_END, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
+  KC_ESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,  _______, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, LCA(KC_DEL),
+  KC_DEL,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,   KC_LEFT,   KC_DOWN,    KC_UP,    KC_RGHT, KC_RCBR, KC_PSCR,
+   _______, KC_F9,   KC_F10,   KC_F11,   KC_F12,  _______,  KC_HOME,  KC_PGDN, KC_PGUP, KC_END, KC_END,  _______,
+  _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
 /* Raise
@@ -136,8 +136,8 @@ MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC
  */
 [_RAISE] = LAYOUT_preonic_grid(
   KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    LCA(KC_DEL),
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_PSCR, KC_DEL,
-  KC_DEL,  KC_4,   KC_5,   KC_6,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,   KC_0,  LCA(KC_DEL),
+  RCS(KC_V),  KC_4,   KC_5,   KC_6,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_7,   KC_8,   KC_9,   KC_0,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
@@ -157,12 +157,11 @@ MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC
  */
 [_ADJUST] = LAYOUT_preonic_grid(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
-  _______, RESET,   DEBUG,   _______, _______, _______, _______, TERM_ON, TERM_OFF,_______, _______, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK, DVORAK,  _______, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL ,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  COLEMAK,  DVORAK,  PLOVER,  _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
-
 
 };
 
