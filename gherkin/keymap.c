59f5ea9eb47b6652db6871437e813ceb82465cee
Andy Ferguson <andy@teknostatik.org>
2022-12-08 20:32:01 +0000

    Add period and better numbers

--- gherkin/keymap.c
+++ gherkin/keymap.c
@@ -7,7 +7,7 @@
 #define FN5_B       LT(5, KC_B)
 #define CTL_Z       CTL_T(KC_Z)
 #define ALT_X       ALT_T(KC_X)
-#define ALT_N       ALGR_T(KC_N)
+#define ALT_N       ALT_T(KC_N)
 #define CTL_M       RCTL_T(KC_M)
 #define SFT_ENT     RSFT_T(KC_ENT)
 
@@ -15,14 +15,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ESC,
-    CTL_Z,   ALT_X,   FN3_C,   FN4_V,   FN2_BSPC,FN1_SPC, FN5_B,   ALT_N,   CTL_M,   SFT_ENT
+    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    SFT_ENT,
+    CTL_Z,   ALT_X,   FN3_C,   FN4_V,   FN2_BSPC,FN1_SPC, FN5_B,   ALT_N,   CTL_M,   KC_DOT
   ),
 
   [1] = LAYOUT_ortho_3x10(
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______, _______
   ),
 
   [2] = LAYOUT_ortho_3x10(
