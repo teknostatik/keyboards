c34afd20b512469c007a44fe6a886eae9d377940
Andy Ferguson <andy@teknostatik.org>
2022-12-08 21:54:36 +0000

    Getting closer

--- gherkin/keymap.c
+++ gherkin/keymap.c
@@ -20,9 +20,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_ortho_3x10(
-    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______, _______
+    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_BSPC,
+    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_SCLN,
+    KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______, KC_COMM
   ),
 
   [2] = LAYOUT_ortho_3x10(
