ecb1a622c65093d5f02b861ac53241513b59da1a
Andy Ferguson <andy@teknostatik.org>
2022-12-09 07:56:53 +0000

    Tab, Esc and arrows

--- gherkin/keymap.c
+++ gherkin/keymap.c
@@ -20,8 +20,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [1] = LAYOUT_ortho_3x10(
-    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_BSPC,
-    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_SCLN,
+    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_ESC,  KC_TAB,  KC_BSPC,
+    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN,   KC_UP, KC_RGHT,   KC_SCLN,
     KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______, KC_COMM
   ),
 
