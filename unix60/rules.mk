c9f507aa5acfd759bb455c0c5f6db1768e5e9114
Andy Ferguson <andy@teknostatik.org>
2023-01-03 11:25:22 +0000

    Reverting

--- unix60/rules.mk
+++ unix60/rules.mk
@@ -1,9 +1,8 @@
 # MCU name
-# MCU = atmega32u4
+MCU = atmega32u4
 
 # Bootloader selection
-# BOOTLOADER = caterina
-BOOTLOADER = rp2040
+BOOTLOADER = caterina
 
 # Build Options
 #   change yes to no to disable
