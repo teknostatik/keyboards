0f182cef8c416c517c3895a508d4fcdc82fb888e
Andy Ferguson <andy@teknostatik.org>
2023-01-31 22:25:03 +0000

    Fixed right hand side layer 0

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -25,10 +25,10 @@ enum ferris_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* QWERTY */
-    KC_B,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_Z, KC_X,    KC_C,    KC_V,    KC_Q,            KC_N,    KC_M,  KC_COMM, KC_DOT, MT(MOD_RSFT, KC_ENT),
-                      KC_BSM1, MT(MOD_LALT, KC_TAB), KC_SPM2, MT(MOD_LCTL, KC_F5)
+    KC_B,    KC_W,    KC_E,    KC_R,    KC_T,            KC_P,    KC_O,  KC_I,    KC_U,   KC_ENT,
+    KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_N,    KC_SCLN,  KC_K,    KC_J,   KC_H,
+    KC_Z, KC_X,    KC_C,    KC_V,    KC_Q,            KC_KC_SPM2,    KC_Y,  KC_DOT, MT(MOD_LCTL, KC_F5), KC_M,
+                      KC_BSM1, MT(MOD_LALT, KC_TAB), KC_L, KC_COMM
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
