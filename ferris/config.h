f7e82d219f1fff7fd5a600f056982adcd13cd963
Andy Ferguson <andy@teknostatik.org>
2023-01-31 22:43:48 +0000

    Patch

--- ferris/config.h
+++ ferris/config.h
@@ -54,3 +54,18 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
 #endif
+
+#undef DIRECT_PINS
+#define DIRECT_PINS { \
+    { D7, F7, F6, F5, F4 }, \
+    { B1, B3, B2, B6, D3 }, \
+    { D1, D0, D4, C6, E6 }, \
+    { B4, B5, NO_PIN, NO_PIN, NO_PIN } \
+}
+#undef DIRECT_PINS_RIGHT
+#define DIRECT_PINS_RIGHT { \
+    { F4, F5, F6, F7, D7 }, \
+    { D3, B6, B2, B3, B1 }, \
+    { E6, C6, D4, D0, D1 }, \
+    { B5, B4, NO_PIN, NO_PIN, NO_PIN } \
+}
