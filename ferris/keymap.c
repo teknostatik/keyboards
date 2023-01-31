5499c6c2c85543b056cfb77a97a55c7b30ea7e4f
Andy Ferguson <andy@teknostatik.org>
2023-01-31 22:06:38 +0000

    Fix left hand side

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -25,10 +25,10 @@ enum ferris_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* QWERTY */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
+    KC_B,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
     KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, MT(MOD_RSFT, KC_ENT),
-                      MT(MOD_LALT, KC_TAB), KC_BSM1, KC_SPM2, MT(MOD_LCTL, KC_F5)
+    KC_Z, KC_X,    KC_C,    KC_V,    KC_Q,            KC_N,    KC_M,  KC_COMM, KC_DOT, MT(MOD_RSFT, KC_ENT),
+                      KC_BSM1, MT(MOD_LALT, KC_TAB), KC_SPM2, MT(MOD_LCTL, KC_F5)
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
