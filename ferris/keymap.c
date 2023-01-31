5a630390e85f287e1901ff3f722d6efd35c68414
Andy Ferguson <andy@teknostatik.org>
2023-01-31 14:41:59 +0000

    Mirroring my Corne layout as much as possible

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -26,22 +26,22 @@ enum ferris_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* QWERTY */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_CTLA, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
-    KC_LSHZ, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_RLSH,
-                                    KC_CLGV, KC_BSM1, KC_SPM2, KC_GUTA
+    KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN,
+    KC_Z, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, MT(MOD_RSFT, KC_ENT),
+                      MT(MOD_LALT, KC_TAB), KC_BSM1, KC_SPM2, MT(MOD_LCTL, KC_F5)
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
-    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    QK_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_ENT,
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,            RCS(KC_DOWN), LCA(KC_DEL), KC_ESC, KC_TAB, KC_BSPC,
+    KK_F5, KC_F6, KC_F7, KC_F8, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_F9, KC_F10, KC_F11, KC_F12, KC_BTN2,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_SLSH,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, QK_BOOT,
+    KC_1,   KC_2,   KC_3,   KC_F4,   KC_F5,           KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,
+    KC_4,  KC_5, KC_6, KC_TRNS, KC_TRNS,             KC_TRNS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,
+    KC_7, KC_8, KC_9, KC_0, KC_TRNS,                KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
