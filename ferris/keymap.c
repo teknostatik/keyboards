a4f8cd58aa261db38bb37ce9a7c4ae58b60a4113
Andy Ferguson <andy@teknostatik.org>
2023-02-03 15:07:48 +0000

    More transparencies

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -32,9 +32,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_5,            RCS(KC_DOWN), LCA(KC_DEL), KC_ESC, KC_TAB, KC_BSPC,
-    KC_F5, KC_F6, KC_F7, KC_F8, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
-    KC_F9, KC_F10, KC_F11, KC_F12, KC_BTN2,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_SLSH,
+    KC_F1,    KC_F2,    KC_F3,    KC_TRNS,    KC_5,            RCS(KC_DOWN), LCA(KC_DEL), KC_ESC, KC_TAB, KC_BSPC,
+    KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_SLSH,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
   ),
 
