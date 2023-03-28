871f263be8dd61a7996b19f48df8816582e0ca85
Andy Ferguson <andy@teknostatik.org>
2023-03-28 11:53:28 +0100

    Change tapping term to match the Corne

--- ferris/config.h
+++ ferris/config.h
@@ -49,7 +49,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 // Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
+#define TAPPING_TERM 100
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
