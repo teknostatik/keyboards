5cadd5a0d51da2ddd0c73722c2709d7d5c4d4ff4
Andy Ferguson <andy@teknostatik.org>
2023-04-02 08:57:50 +0100

    Adding a shortcut to split terminals

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_1, KC_2, KC_3, KC_TRNS, KC_PSCR,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,           KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+    KC_4, KC_5, KC_6, KC_TRNS, RCS(KC_ENT),       KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
     KC_7, KC_8, KC_9, KC_0,    G(KC_L),           KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
                       KC_TRNS, KC_TRNS,           KC_TRNS, KC_LGUI
   )
