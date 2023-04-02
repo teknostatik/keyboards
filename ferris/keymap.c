034479068bf68bbf08c911be57aff81dcea5c1da
Andy Ferguson <andy@teknostatik.org>
2023-04-02 08:52:55 +0100

    Tidying up

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -25,24 +25,24 @@ enum ferris_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( /* QWERTY */
-    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
+    KC_Q, KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
     KC_A, KC_S,    KC_D,    KC_F,    KC_G,            KC_H,    KC_J,  KC_K,    KC_L,   KC_QUOT,
     KC_Z, KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, MT(MOD_RSFT, KC_ENT),
-                      MT(MOD_LALT, KC_TAB), KC_BSM1, KC_SPM2, MT(MOD_LCTL, KC_F5)
+            MT(MOD_LALT, KC_TAB), KC_BSM1,            KC_SPM2, MT(MOD_LCTL, KC_F5)
   ),
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
-    KC_F1,    KC_F2,    KC_F3, KC_F4, KC_TRNS,            RCS(KC_DOWN), LCA(KC_DEL), KC_ESC, KC_TAB, KC_BSPC,
-    KC_F5, KC_F6, KC_F7, KC_F8, KC_TRNS,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_SCLN,
-    KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,         KC_HOME, KC_PGDN, KC_PGUP, KC_END, KC_SLSH,
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  
+    KC_F1, KC_F2, KC_F3,   KC_F4, KC_TRNS,        RCS(KC_DOWN), LCA(KC_DEL), KC_ESC,  KC_TAB,  KC_BSPC,
+    KC_F5, KC_F6, KC_F7,   KC_F8, KC_TRNS,        KC_LEFT,      KC_DOWN,     KC_UP,   KC_RGHT, KC_SCLN,
+    KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,       KC_HOME,      KC_PGDN,     KC_PGUP, KC_END,  KC_SLSH,
+                          KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS  
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
-    KC_1,   KC_2,   KC_3,   KC_TRNS,  KC_PSCR,           KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,
-    KC_4,  KC_5, KC_6, KC_TRNS, KC_TRNS,             KC_TRNS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC,
-    KC_7, KC_8, KC_9, KC_0, G(KC_L),                KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
-                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_LGUI
+    KC_1, KC_2, KC_3, KC_TRNS, KC_PSCR,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,           KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+    KC_7, KC_8, KC_9, KC_0,    G(KC_L),           KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
+                      KC_TRNS, KC_TRNS,           KC_TRNS, KC_LGUI
   )
 };
 
