79d41b73e19466d9062a8ed7d58fdf2d97ee2c04
Andy Ferguson <andy@teknostatik.org>
2023-04-02 21:38:58 +0100

    Moving a few things on layers

--- ferris/keymap.c
+++ ferris/keymap.c
@@ -33,15 +33,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT( /* [> LOWER <] */
     KC_F1, KC_F2, KC_F3,   KC_F4, KC_TRNS,        RCS(KC_DOWN), LCA(KC_DEL), QK_GESC,  KC_TAB,  KC_BSPC,
-    KC_F5, KC_F6, KC_F7,   KC_F8, KC_TRNS,        KC_LEFT,      KC_DOWN,     KC_UP,   KC_RGHT, KC_SCLN,
-    KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS,       KC_HOME,      KC_PGDN,     KC_PGUP, KC_END,  KC_SLSH,
+    KC_F5, KC_F6, KC_F7,   KC_F8, RCS(KC_ENT),    KC_LEFT,      KC_DOWN,     KC_UP,   KC_RGHT, KC_SCLN,
+    KC_F9, KC_F10, KC_F11, KC_F12, G(KC_L),       KC_HOME,      KC_PGDN,     KC_PGUP, KC_END,  KC_SLSH,
                           KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS  
   ),
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_1, KC_2, KC_3, KC_TRNS, KC_PSCR,           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_4, KC_5, KC_6, KC_TRNS, RCS(KC_ENT),       KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
-    KC_7, KC_8, KC_9, KC_0,    G(KC_L),           KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
+    KC_4, KC_5, KC_6, KC_TRNS, RCS(KC_V),         KC_TRNS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,
+    KC_7, KC_8, KC_9, KC_0,    RCS(KC_DOWN),      KC_TRNS, KC_NUHS, KC_NUBS, KC_TRNS, QK_BOOT,
                       KC_TRNS, KC_TRNS,           KC_TRNS, KC_LGUI
   )
 };
