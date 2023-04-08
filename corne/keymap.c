b2f39ad350bed0419e9e2ab449f1c83e1647abca
Andy Ferguson <andy@teknostatik.org>
2023-04-08 19:24:46 +0100

    Tidying up

--- corne/keymap.c
+++ corne/keymap.c
@@ -34,13 +34,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
+      KC_TAB,    KC_Q,   KC_W,   KC_E,    KC_R,  KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_SESC,  KC_A, KC_S,  KC_D, KC_F, KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+      KC_SESC,   KC_A,   KC_S,   KC_D,    KC_F,  KC_G,                           KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_SBSL, KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_SHEN,
+      KC_SBSL,   KC_Z,   KC_X,   KC_C,    KC_V,  KC_B,                           KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_SHEN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                   KC_ALTA, MO(1), KC_BSPC,                       KC_SPC, MO(2), KC_CTF5
+                                 KC_ALTA, MO(1), KC_BSPC,                        KC_SPC,  MO(2),   KC_CTF5
                                       //`--------------------------'  `--------------------------'
 
   ),
