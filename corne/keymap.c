4c0ee3314163645d39c8f5de274fe7942ce0d760
Andy Ferguson <andy@teknostatik.org>
2023-04-08 13:12:33 +0100

    Two more definitions

--- corne/keymap.c
+++ corne/keymap.c
@@ -28,15 +28,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KC_SCV RCS(KC_V)
 #define KC_SPM2 LT(2, KC_SPC)
 #define KC_BSM1 LT(1, KC_BSPC)
+#define KC_SESC MT(MOD_LCTL, KC_ESC)
+#define KC_SBSL MT(MOD_LSFT, KC_NUBS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      MT(MOD_LCTL, KC_ESC),  KC_A, KC_S,  KC_D, KC_F, KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+      KC_SESC,  KC_A, KC_S,  KC_D, KC_F, KC_G,                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      MT(MOD_LSFT, KC_NUBS), KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_SHEN,
+      KC_SBSL, KC_Z, KC_X, KC_C, KC_V, KC_B,                        KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_SHEN,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                    KC_ALTA, MO(1), KC_BSPC,                       KC_SPC, MO(2), KC_CTF5
                                       //`--------------------------'  `--------------------------'
