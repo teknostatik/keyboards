86b57735cb06b12b3df4cb74d46c8ce3334405bf
Andy Ferguson <andy@teknostatik.org>
2023-04-21 13:05:11 +0100

    Define handedness

--- ferris_sweep/config.h
+++ ferris_sweep/config.h
@@ -38,6 +38,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 
+#define MASTER_RIGHT
+
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
