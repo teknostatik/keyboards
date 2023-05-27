ae1bfd044403ca3111592f2aed0851c6bd766163
Andy Ferguson <andy@teknostatik.org>
2023-05-27 15:54:47 +0100

    Enable autoshift

--- Tidbit/rules.mk
+++ Tidbit/rules.mk
@@ -8,10 +8,11 @@ CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 ENCODER_ENABLE = yes
 LTO_ENABLE = yes
+AUTOSHIFT_ENABLE = yes
 
 # Project specific files
 SRC += common/bitc_led.c \
