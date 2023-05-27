9d58011dfd0b8530a56f1539f243387f8529291f
Andy Ferguson <andy@teknostatik.org>
2023-05-27 20:12:45 +0100

    More audio stuff

--- tidbit/keymap.c
+++ tidbit/keymap.c
@@ -25,7 +25,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                            KC_PSLS, KC_PAST, KC_PMNS, 
+                            KC_SPC,  KC_PAST, KC_MINS, 
     KC_VOLD, KC_VOLU, KC_P7, KC_P8,   KC_P9,   KC_PPLS, 
     KC_MPRV, KC_MNXT, KC_P4, KC_P5,   KC_P6,   KC_PPLS, 
     KC_LEFT, KC_RGHT, KC_P1, KC_P2,   KC_P3,   KC_PENT, 
