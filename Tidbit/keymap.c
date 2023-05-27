4ad44dedf2391cf5f4503ca703683dad1249e617
Andy Ferguson <andy@teknostatik.org>
2023-05-27 16:00:16 +0100

    First stab at a keymap

--- Tidbit/keymap.c
+++ Tidbit/keymap.c
@@ -23,11 +23,11 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                            KC_PSLS, KC_PAST, KC_PMNS, 
-    KC_VOLD, KC_VOLU, KC_P7, KC_P8,   KC_P9,   KC_PPLS, 
-    KC_MPRV, KC_MNXT, KC_P4, KC_P5,   KC_P6,   KC_PPLS, 
-    KC_LEFT, KC_RGHT, KC_P1, KC_P2,   KC_P3,   KC_PENT, 
-    KC_TRNS, KC_TRNS, KC_P0, KC_P0,   KC_PDOT, KC_PENT  
+                            KC_PSLS,  KC_ESC,  KC_GRV, 
+    KC_VOLD, KC_VOLU, KC_1, KC_2,   KC_3,   KC_MINS, 
+    KC_MPRV, KC_MNXT, KC_4, KC_5,   KC_6,   KC_EQL, 
+    KC_LEFT, KC_RGHT, KC_7, KC_8,   KC_9,   KC_0, 
+    KC_TRNS, KC_TRNS, KC_LCTRL, KC_LALT,   KC_LGUI, KC_PENT  
     ),
 
     [_FUNC] = LAYOUT(
