0a765c64612cf3a097b471b667cb5f9f59b61482
Andy Ferguson <andy@teknostatik.org>
2023-05-27 19:26:47 +0100

    Pause?

--- tidbit/keymap.c
+++ tidbit/keymap.c
@@ -23,7 +23,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
-                            KC_PSLS,  KC_ESC,  KC_BSPC, 
+                            KC_PAUS,  KC_ESC,  KC_BSPC, 
     KC_VOLD, KC_VOLU, KC_1, KC_2,   KC_3,   KC_MINS, 
     KC_MPRV, KC_MNXT, KC_4, KC_5,   KC_6,   KC_EQL, 
     KC_LEFT, KC_RGHT, KC_7, KC_8,   KC_9,   KC_0, 
