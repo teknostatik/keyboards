943447122b1836c9b4bde472c847e347e22ef5a7
Andy Ferguson <andy@teknostatik.org>
2023-08-27 10:12:06 +0100

    Moving a few things around

--- gherkin/keymap.c
+++ gherkin/keymap.c
@@ -25,25 +25,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho_3x10(
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,  KC_P,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  SFT_ENT,
-    CTL_Z,   ALT_X,   FN3_C,   FN4_V,   FN2_BSPC,FN1_SPC, FN5_B,   KC_N,   KC_M,   KC_DOT
+    KC_Z,   KC_X,   KC_C,   FN4_V,   FN2_BSPC,   FN1_SPC, KC_B,   KC_N,   KC_M,   KC_DOT
   ),
 
   [1] = LAYOUT_ortho_3x10(
-    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_ESC,  KC_TAB,  KC_BSPC,
-    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN,   KC_UP, KC_RGHT,   KC_SCLN,
-    KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______, KC_COMM
+    KC_TAB, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
+    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_BOOT,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_QUOT
   ),
 
   [2] = LAYOUT_ortho_3x10(
-    KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-    KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, KC_GRV,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+    KC_1,   KC_2,    KC_3, KC_4,    KC_5,    KC_6,    KC_7,    KC_ESC,  KC_TAB,  KC_BSPC,
+    KC_4,   KC_5,   KC_6,  KC_F4,   KC_F5,   KC_LEFT,   KC_DOWN,   KC_UP, KC_RGHT, KC_SCLN,
+    KC_7,   KC_8,   KC_9,  KC_0 ,   KC_DEL,  _______, _______, _______, _______,  KC_COMM
   ),
 
   [3] = LAYOUT_ortho_3x10(
     _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_TAB,  _______, _______, _______, _______, KC_COMM, KC_DOT,  KC_SLSH, KC_SCLN, KC_QUOT,
-    _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    _______, _______, _______, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
   [4] = LAYOUT_ortho_3x10(
